a37803df355b2f2f052518b0965f08b38ddb7d51
Ranjith Zachariah <ranjith.zachariah@gmail.com>
2021-04-04 00:22:32 -0400

    Initial commit

---
+++ testproject/urls.py
@@ -0,0 +1,28 @@
+"""testproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.conf.urls import url, include
+
+@api_view(['GET','POST'])
+def hello_world(request):
+        if request.method == 'POST':
+                return Response({"message": "Got some data!", "data": request.data})
+        return Response({"message": "Hello, world!"})
+
+urlpatterns = [
+  url(r'^hello/$', hello_world),
+]
